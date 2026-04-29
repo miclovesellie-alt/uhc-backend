404b0db097552000202b33c345a2c64705875e4c
miclovesellie-alt <miclovesellie@gmail.com>
2026-04-29 04:03:07 +0000

    clean project for deployment

--- routes/points.js
+++ routes/points.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { addPoints } = require("../controllers/pointsController");
-const authMiddleware = require("../middleware/auth.middleware");
+const { authMiddleware } = require("../middleware/auth.middleware");
 
 router.post("/add", authMiddleware, addPoints);
 
